5295bbcf917c1dfda96a603bcbedc0957accd9fc
superfloyd <superfloyd@gmail.com>
2013-03-27 10:58:14 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyagablogBlog::Application.config.secret_token = '61b2f1e1b721e8ebbd9f0e458318c15d2d22b5838a02672093edb16d6fbe1830227f63ef4d369b7f7781b2d212b7ae184ef4c20386f928a5bf727d526024e264'
